aec6a5606f99d9fd237ba2fdefb914e58b2b3332
Lisandrad <lisandraestefanid@gmail.com>
2022-11-23 09:08:27 -0400

    First React interactive calculator

---
+++ calculator/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import freecodecamp from './images/freecodecamp.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+        src={freecodecamp}
+        className='freecodecamp-=logo'
+        alt='Logo de freecodecamp' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
